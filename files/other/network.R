20508ce41d7413b75f6f492beb7ae90fe5d8756d
CamilleLeclerc <camille.leclerc@ymail.com>
2024-10-19 13:26:14 +0200

    Updates

--- files/other/network.R
+++ files/other/network.R
@@ -53,3 +53,76 @@ ggraph(g, layout = 'fr') +
   ggtitle("Snapshot view of my collaborations [Update October 2024]") +
   theme(plot.title = element_text(face = "bold", size = 10, hjust = 0.5))
 
+
+
+##################################################
+##Word cloud based on the abstracts of my papers##
+##################################################
+#https://cran.r-project.org/web/packages/wordcloud2/vignettes/wordcloud.html#install-wordcloud2
+#https://superstatisticienne.fr/wordcloud-avec-r/
+
+# Load packages
+library(readxl)
+library(dplyr)
+library(wordcloud2)
+library(tm)
+library(SnowballC)
+library(RColorBrewer)
+library(htmlwidgets)
+library(webshot2)
+
+# Load the data
+info_abstracts <- read_excel("files/other/info_abstracts.xlsx")
+head(info_abstracts)
+
+# Extract abstracts and combine them into a single text
+abstracts_text <- paste(info_abstracts$Abstract, collapse = " ")
+
+# Create a text corpus
+corpus <- Corpus(VectorSource(abstracts_text))
+
+# Text cleaning (lower case, removal of stopwords, punctuation, etc.)
+corpus <- corpus %>%
+  tm_map(content_transformer(tolower)) %>%
+  tm_map(removePunctuation) %>%
+  tm_map(removeNumbers) %>%
+  tm_map(removeWords, stopwords("en")) %>%
+  tm_map(stripWhitespace)
+
+# Create a term matrix
+dtm <- TermDocumentMatrix(corpus)
+matrix <- as.matrix(dtm)
+
+# Count words and create a data frame
+word_freq <- sort(rowSums(matrix), decreasing = TRUE)
+data_wordcloud <- data.frame(word = names(word_freq), freq = word_freq)
+data_wordcloud <- data_wordcloud %>% dplyr::filter(freq > 4)
+unique(data_wordcloud$word)
+data_wordcloud <- data_wordcloud %>% dplyr::filter(!word %in% c("found", "also", "high", "new", "will", "calvescens", "can", "however", "french", "importance", "infection", "mostly", "studies", "three", "well", "yet", "suggest", "therefore", "using", "antelope", "due", "one", "two", "whether", "within"))
+
+continuous_colors <- colorRampPalette(brewer.pal(11, "Spectral"))(nrow(data_wordcloud)/2)
+
+# Create the word clouds
+wordcloud <- wordcloud2(data_wordcloud, size = 0.5, color = continuous_colors,
+           backgroundColor = "white") 
+wordcloud
+
+# Add a title using `htmlwidgets` and HTML
+wordcloud <- htmlwidgets::prependContent(
+  wordcloud,
+  htmltools::tags$h1("Word cloud based on the abstracts of my papers [Update October 2024]",
+                     style = "text-align:center; color:black; font-family:sans-serif; font-size:14px; font-weight:bold;")
+)
+wordcloud
+
+
+#continuous_colors <- colorRampPalette(c("#000000", "#5E4FA2", "#3288BD", "#66C2A5", "#ABDDA4", "#E6F598", "#FFFFBF", "#FEE08B", "#FDAE61", "#F46D43", "#D53E4F", "#9E0142"))(nrow(data_wordcloud)/2)
+#wordcloud(
+#  words = data_wordcloud$word,
+#  freq = data_wordcloud$freq,
+#  scale = c(3, 0.5), # Taille max et min des mots
+#  colors = continuous_colors,
+#  random.order = FALSE,
+#  rot.per = 0.35, # Pourcentage de mots affichés verticalement
+#  use.r.layout = FALSE
+#)
